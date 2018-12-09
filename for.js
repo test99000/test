ea3c2f9cdd5c9531b5afa19eaf3f0c752f106a40
test99000 <45726883+test99000@users.noreply.github.com>
2018-12-08 22:00:08 -0800

    Update for.js

--- for.js
+++ for.js
@@ -1,14 +1,3 @@
-client.on('message', msg => {
-
-    if (msg.content == '$join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => {
-    client.channels.get("521197760993689600").join(); 
-    });
+client.on('ready',async () => {
+  client.channels.find(ch => ch.id === "521197760993689600" && ch.type === 'voice').join();
+});
